320a3c7ac60403a5d6ad4d930b9fc665e8468c68
sanket <sanket.savaliya@nascentinfo.com>
2025-07-30 16:14:42 +0530

    active routeing menu feature add

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,11 +6,11 @@ import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { GeoserverComponent } from './components/geoserver/geoserver.component';
 
 export const routes: Routes = [
+  { path: '', redirectTo: 'layer-switcher', pathMatch: 'full' },
   { path: 'layer-switcher', component: LayerSwitchderComponent },
   { path: 'attribute-table', component: AttributeTableComponent },
   { path: 'query-module', component: QueryModuleComponent },
   { path: 'dashboard', component: DashboardComponent },
   { path: 'geoserver', component: GeoserverComponent },
-  { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
-  { path: '**', redirectTo: 'dashboard' }
+  { path: '**', redirectTo: 'layer-switcher' }
 ];
--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -1,14 +1,16 @@
 import { Component, ComponentRef, EventEmitter, Output, ViewChild, ViewContainerRef } from '@angular/core';
-import { Router } from '@angular/router';
+import { NavigationEnd, Router, RouterModule } from '@angular/router';
 import { RouterLink } from '@angular/router';
 import { ButtonModule } from 'primeng/button';
 import { InputTextModule } from 'primeng/inputtext';
 import { DrawerModule } from 'primeng/drawer';
 import { ProfileComponent } from '../../components/profile/profile.component';
+import { filter } from 'rxjs';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [ButtonModule, InputTextModule, DrawerModule,ProfileComponent],
+  imports: [CommonModule, ButtonModule, InputTextModule, DrawerModule, ProfileComponent, RouterModule],
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
@@ -16,7 +18,16 @@ export class HeaderComponent {
   @Output() toggleSidebar = new EventEmitter<void>();
   @Output() panelSelect = new EventEmitter<string>();
   isUserPanelVisible: boolean = false;
-  constructor(private router: Router) { }
+  currentRoute: string = '';
+
+  constructor(private router: Router) {
+    this.router.events
+      .pipe(filter((event) => event instanceof NavigationEnd))
+      .subscribe(() => {
+        this.currentRoute = this.router.url;
+      });
+  }
+
   openPanel(panel: string) {
     this.panelSelect.emit(panel);
     this.router.navigate(['/' + panel]);
@@ -25,6 +36,18 @@ export class HeaderComponent {
 
   openUserPanel() {
     this.isUserPanelVisible = true;
+  }
+
+  menuButtons = [
+    { label: 'Layer Switcher', path: 'layer-switcher' },
+    { label: 'Dashboard', path: 'dashboard' },
+    { label: 'Attribute Table', path: 'attribute-table' },
+    { label: 'Query Module', path: 'query-module' },
+    { label: 'Geoserver', path: 'geoserver' }
+  ];
+
+  isActive(path: string): boolean {
+    return this.currentRoute.includes(path);
   }
 
 }
